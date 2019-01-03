f59e7898991b269dac64fa0e0b168445fb21d954
Josh Weisman <josh.m.weisman@gmail.com>
2019-01-03 17:11:18 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,85 @@
-# almarestapi-node-lib
+# Alma REST API Node Library
+
+This library makes it easy to call the Ex Libris Alma REST APIs in Node.js.
+
+## Installation
+`npm install almarestapi-lib --save` 
+
+## API Key
+The library expects that the API key be available in an environment variable called `ALMA_API_KEY`. The API host can be optionally stored in an environment variable called `ALMA_API_HOST`. The defaut is https://api-na.hosted.exlibrisgroup.com/almaws/v1'.
+
+You can call your script with the API key in the command line, as follows:
+```
+ALMA_API_KEY=l7xxabcdefghijklmnopqrstuvwxyz node test.js 
+```
+
+## Usage
+To being using the library, require it at the top of your Node file:
+```
+var alma = require ('exl-alma-api-lib');
+```
+
+### Async/Await
+The library supports usage of `async/await`. The functions which return a Promise end in the letter 'p', i.e. `getp`.
+```
+var alma = require ('almarestapi-lib');
+
+async function main() {
+  try {
+    console.log(await alma.getp('/users/joshw?view=brief'));
+  } catch (e) {
+    console.error('ERROR: ', e.message);
+  }
+}
+
+main();
+```
+
+## Working with JSON
+The following functions work with JSON:
+
+*Retrieving JSON with `getp`*
+```
+await alma.getp('/users/joshw?view=brief');
+```
+*Updating JSON with `putp`*
+```
+var user = await alma.getp('/users/joshw');
+user.middle_name = 'Max';
+console.log(await alma.putp('/users/joshw', user);
+```
+*Creating a record with `postp`*
+```
+var user = { first_name: 'Josh', last_name: 'Testman', account_type:{value: 'INTERNAL'}};
+console.log(await alma.postp('/users', user));
+```
+
+*Deleting with `deletep`*
+```
+alma.deletep('/users/4101979450000561');
+```
+
+## Working with XML
+It is preferable to work with XML when calling APIs which relate to BIB records. You can use the XML functions to work with these APIs.
+
+*Retrieving a BIB record with `getXmlp`*
+```
+var bib = await alma.getXmlp('/bibs/9981140900561');
+```
+
+*Updating and creating a BIB record*
+You can use the `putXmlp` and `postXmlp` functions to update and create BIB records respectively.
+```
+var dom        = require('xmldom').DOMParser;
+var xpath      = require('xpath');
+const XPATH_TITLE = '/bib/record/datafield[@tag="245"]/subfield[@code="a"]';
+
+var bib = await alma.getXmlp('/bibs/991439870000541');
+bib = new dom().parseFromString(bib);
+xpath.select(XPATH_TITLE, bib)[0]
+  .firstChild.data='History curatorship';
+console.log(await alma.putXmlp('/bibs/991439870000541', bib));      
+```
+
+## Issues
+Feel free to report issues in this repository.
---
+++ alma.js
@@ -0,0 +1,148 @@
+var request    = require('request');
+var xpath      = require('xpath');
+var dom        = require('xmldom').DOMParser;
+var serializer = require('xmldom').XMLSerializer;
+
+const API_PATH = process.env.ALMA_API_PATH || 'https://api-na.hosted.exlibrisgroup.com/almaws/v1';
+const API_KEY  = process.env.ALMA_API_KEY;
+const ALMAWS_NS = {"alma": "http://com/exlibris/urm/general/xmlbeans"};
+const XPATH_ALMAWS_ERROR = '/alma:web_service_result/alma:errorList/alma:error/alma:errorMessage';
+
+function performRequestPromise(endpoint, method, data, contentType='json') {
+  return new Promise(function (resolve, reject) {
+    performRequest(endpoint, method, data, function(err, data) {
+      if (err) reject(err);
+      else resolve(data);
+    }, contentType);
+  });
+}
+
+function performRequest(endpoint, method, data, callback, contentType='json') {
+  var dataString;
+  var headers = {
+    'Authorization': 'apikey ' + API_KEY,
+    'Accept': (contentType=='json' ? 
+      'application/json' : 'application/xml') 
+  };
+
+  var options = {
+    uri: (endpoint.substring(0,4) == 
+      'http' ? '' : API_PATH) + endpoint,
+    method: method,
+    headers: headers,
+  };
+
+  if (method != 'GET') {
+    if (contentType=='json') {
+      dataString = JSON.stringify(data);
+      headers['Content-Type'] = 'application/json';
+    } else {
+      dataString = typeof data == 'object' ? 
+        new serializer().serializeToString(data) : data;
+      headers['Content-Type'] = 'application/xml';
+    }
+    headers['Content-Length'] = dataString.length;
+    options['body'] = dataString;
+  }
+
+  request(
+    options,
+    function(err, response, body) {
+      var obj;
+      try {
+        obj = (contentType=='json' ? 
+          JSON.parse(body) : new dom().parseFromString(body));
+        if (!err && ('' + response.statusCode).match(/^[4-5]\d\d$/)) {
+          var message;
+          try {
+            if (contentType=='json') {
+              message = obj.errorList.error[0].errorMessage + " (" + 
+                obj.errorList.error[0].errorCode + ")";
+            } else {
+              var select = xpath.useNamespaces(ALMAWS_NS);
+              message = select(XPATH_ALMAWS_ERROR, obj)[0]
+                .firstChild.data;
+            }
+          } catch (e) {
+            message = "Unknown error from Alma.";
+          }
+          err = new Error(message);
+        }
+      } catch (e) {
+        err = e;
+      }
+      callback(err, contentType=='json' ? obj : body);
+    });
+}
+
+exports.get = function(url, callback) {
+  performRequest(url, 'GET', null, 
+    function(err, data) {
+      if (err) callback(err);
+      else callback(null, data);
+    });
+};
+
+exports.getp = function(url) {
+  return performRequestPromise(url, 'GET', null);
+}
+
+exports.getXml = function(url, callback) {
+  performRequest(url, 'GET', null, 
+    function(err, data) {
+      if (err) callback(err);
+      else callback(null, data);
+    }, 'xml');
+};
+
+exports.getXmlp = function(url) {
+  return performRequestPromise(url, 'GET', null, 'xml');
+}
+
+exports.post = function(url, data, callback) {
+  performRequest(url, 'POST', data, 
+    function(err, data) {
+      if (err) callback(err);
+      else callback(null, data);
+    });
+};
+
+exports.postp = function(url, data) {
+  return performRequestPromise(url, 'POST', data);
+}
+
+exports.put = function(url, data, callback) {
+  performRequest(url, 'PUT', data, 
+    function(err, data) {
+      if (err) callback(err);
+      else callback(null, data);
+    });
+};
+
+exports.putp = function(url, data) {
+  return performRequestPromise(url, 'PUT', data);
+}
+
+exports.putXml = function(url, data, callback) {
+  performRequest(url, 'PUT', data, 
+    function(err, data) {
+      if (err) callback(err);
+      else callback(null, data);
+    }, 'xml');
+};
+
+exports.putXmlp = function(url, data) {
+  return performRequestPromise(url, 'PUT', data, 'xml');
+}
+
+exports.delete = function(url, callback) {
+  performRequest(url, 'DELETE', null, 
+    function(err, data) {
+      if (err) callback(err);
+      else callback(null, data);
+    });
+}
+
+exports.deletep = function(url) {
+  return performRequestPromise(url, 'DELETE', null);
+}
